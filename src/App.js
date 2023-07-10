87fff6496a13812c51bbb55046a2f2cb43e196a4
BadourAlkhateb <bidouralkh@gmail.com>
2023-07-11 01:44:50 +0300

    Fix maintine return issues

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { Button, Container, TextInput, Col, Paper, Text } from '@mantine/core';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { 
   faCircleCheck, faPen, faTrashCan 
@@ -9,24 +9,18 @@ import {
 import './App.css';
 
 function App() {
-
-  
   const [toDo, setToDo] = useState(() => {
-    // Load from local storage when starting up
     const localData = localStorage.getItem('toDo');
     return localData ? JSON.parse(localData) : [];
   });
 
-  useEffect(() => {
-    // Save to local storage whenever toDo changes
-    localStorage.setItem('toDo', JSON.stringify(toDo));
-  }, [toDo]);
-
-  
   const [newTask, setNewTask] = useState('');
   const [updateData, setUpdateData] = useState('');
 
-  
+  useEffect(() => {
+    localStorage.setItem('toDo', JSON.stringify(toDo));
+  }, [toDo]);
+
   const addTask = () => {
     if(newTask) {
       let num = toDo.length + 1; 
@@ -36,13 +30,11 @@ function App() {
     }
   }
 
-  
   const deleteTask = (id) => {
     let newTasks = toDo.filter((task) => task.id !== id);
     setToDo(newTasks);
   }
 
- 
   const markDone = (id) => {
     const newTasks = toDo.map((task) => {
       if (task.id === id){
@@ -53,12 +45,10 @@ function App() {
     setToDo(newTasks);
   }
 
-  
   const cancelUpdate = () => {
     setUpdateData('');
   }
 
-  
   const changeTask = (e) => {
     let newEntry = {
       id: updateData.id,
@@ -68,7 +58,6 @@ function App() {
     setUpdateData(newEntry);
   }
 
-  
   const updateTask = () => {
     let filterRecords = [...toDo].filter( task=>task.id !== updateData.id);
     let updatedObject = [...filterRecords, updateData];
@@ -76,114 +65,56 @@ function App() {
     setUpdateData('');
   }
 
-  
   return (
-    <div className="container App">
-      
-      <br /><br />
+    <Container size={700} className="App">
+      <Text size="xl" weight={700} style={{ marginTop: '2em' }}>Add Your Tasks</Text>
 
-      <h2>Add Your Tasks</h2>
+      <div style={{ marginTop: '2em', marginBottom: '2em' }}>
+        <TextInput
+          label={updateData ? 'Update task' : 'New task'}
+          placeholder="Type here"
+          value={updateData ? updateData.title : newTask}
+          onChange={(event) => updateData ? changeTask(event) : setNewTask(event.currentTarget.value)}
+          rightSection={updateData ?
+            <div style={{ display: 'flex' }}>
+              <Button color="blue" onClick={updateTask}>Update</Button>
+              <Button color="yellow" onClick={cancelUpdate}>Cancel</Button>
+            </div> :
+            <Button color="teal" onClick={addTask}>Add Task</Button>
+          }
+          rightSectionWidth={updateData ? 200 : 100}
+        />
+      </div>
 
-      <br /><br />
-      
-
-      {updateData && updateData ? (
-        <>
-          <div className="row">
-            <div className="col">
-              <input 
-                value={updateData && updateData.title} 
-                onChange={ (e) => changeTask(e) } 
-                className="form-control form-control-lg" 
-              />
-            </div>
-            <div className="col-auto">
-              <button 
-                className="btn btn-lg btn-success mr-20" 
-                onClick={updateTask}
-              >Update</button>
-              <button 
-                className="btn btn-lg btn-warning" 
-                onClick={cancelUpdate}
-              >Cancel</button>
-            </div>
-          </div>
-          <br />
-        </>
-      ) : (
-        <>
-          <div className="row">
-            <div className="col">
-              <input 
-                value={newTask} 
-                onChange={e => setNewTask(e.target.value)} 
-                className="form-control form-control-lg" 
-              />
-            </div>
-            <div className="col-auto">
-              <button 
-                className="btn btn-lg btn-success" 
-                onClick={addTask}
-              >Add Task</button>
-            </div>
-          </div>
-          <br />
-        </>
-      )}
-
-
-     
-      {toDo && toDo.length ? '' : 'No tasks...'}
-      
-      
+      {/* Use Mantine's Paper component to display each task */}
       {toDo && toDo
         .sort((a, b) => a.id > b.id ? 1 : -1)
-        .map( (task, index) => {
-        return(
-          <React.Fragment key={task.id}>
-          
-            <div className="col taskBg">
-              
-              <div 
-                // if task status is true, add class to this div named as done
-                className={ task.status ? 'done' : '' }
-              >
-                {/* Show number of task */}
-                <span className="taskNumber">{index + 1}</span> 
-                <span className="taskText">{task.title}</span>
-              </div>
+        .map( (task, index) => (
+          <Paper padding="md" style={{ marginBottom: '1em' }} key={task.id}>
+            <Text style={{ textDecoration: task.status ? 'line-through' : 'none' }}>
+              <span className="taskNumber">{index + 1}</span> 
+              <span className="taskText">{task.title}</span>
+            </Text>
 
-              <div className="iconsWrap">
-                <span 
-                  onClick={(e) => markDone(task.id)}
-                  title="Completed / Not Completed"
-                >
-                  <FontAwesomeIcon icon={faCircleCheck} />
-                </span>
-                
-                {task.status ? null : (
-                  <span 
-                    title="Edit"
-                    onClick={ () => setUpdateData({ id: task.id, title: task.title, satus: task.status ? true : false }) }
-                  >
-                    <FontAwesomeIcon icon={faPen} />
-                  </span>
-                )}
+            <div className="iconsWrap">
+              <Button color="blue" onClick={() => markDone(task.id)} title="Completed / Not Completed">
+                <FontAwesomeIcon icon={faCircleCheck} />
+              </Button>
 
-                <span 
-                  onClick={() => deleteTask(task.id)}
-                  title="Delete"
-                >
-                  <FontAwesomeIcon icon={faTrashCan} />
-                </span>
-              </div>
+              {task.status ? null : (
+                <Button color="yellow" onClick={() => setUpdateData({ id: task.id, title: task.title, status: task.status })} title="Edit">
+                  <FontAwesomeIcon icon={faPen} />
+                </Button>
+              )}
 
+              <Button color="red" onClick={() => deleteTask(task.id)} title="Delete">
+                <FontAwesomeIcon icon={faTrashCan} />
+              </Button>
             </div>
-                     
-        </React.Fragment>
-        );
-      })}
-    </div>
+          </Paper>
+        ))
+      }
+    </Container>
   );
 }
 
